10370b66b0bcb8b7167f5c26c5c2c70487905d75
diogenesRibeiro <diogenes.riboliveira@gmail.com>
2025-12-29 20:00:22 -0300

    feat: importing new directories

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import fastify, { FastifyInstance } from "fastify";
 
-import { openSupportTicket } from './use_cases/open-support-ticket'
-import { colaborators } from './use_cases/collaborators'
-import { companyRegistration } from "./use_cases/company-registration"
-import { associatedCollaborator } from "./use_cases/associate-collaborator"
-import { companyTokenValidation } from "./use_cases/token-validation";
+import { openSupportTicket } from './use_cases/collaborator-features/open-support-ticket'
+
+import { colaborators } from './use_cases/company-features/collaborators'
+import { companyRegistration } from "./use_cases/company-features/company-registration"
+import { associatedCollaborator } from "./use_cases/company-features/associate-collaborator"
+import { companyTokenValidation } from "./use_cases/company-features/token-validation";
 
 const app: FastifyInstance = fastify()
 
@@ -13,6 +14,7 @@ app.register(
 )
 
 app.register(openSupportTicket)
+
 app.register(colaborators)
 app.register(companyRegistration)
 app.register(associatedCollaborator)
